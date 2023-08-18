52f24aaafedcd59e91c3b5568012ed35c98d7ee2
Ludovic HENRY <badsteam@hotmail.com>
2023-08-18 16:47:27 -0400

    Initial Commit

---
+++ AfterBuild/AfterBuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfterBuildApp.swift
+//  AfterBuild
+//
+//  Created by Ludovic HENRY on 18/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct AfterBuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
