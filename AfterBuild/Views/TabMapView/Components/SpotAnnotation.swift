d295dc7fb6ae327933be14b9d264071f80ebab4b
Ludovic HENRY <badsteam@hotmail.com>
2023-09-23 17:38:23 -0400

    Created a custom Annotation for the Map

---
+++ AfterBuild/Views/TabMapView/Components/SpotAnnotation.swift
@@ -0,0 +1,44 @@
+//
+//  Annotation.swift
+//  AfterBuild
+//
+//  Created by Ludovic HENRY on 23/09/2023.
+//
+
+import SwiftUI
+
+struct SpotAnnotation: View {
+    var location: SpotLocation
+
+    var body: some View {
+        VStack {
+            ZStack{
+                MapBalloonShape()
+                    .frame(width: 100, height: 70)
+                    .foregroundStyle(.brandPrimary)
+
+                Image(uiImage: location.createSquareImage())
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                    .offset(y: -11)
+
+                Text("99")
+                    .font(.system(size: 11, weight: .bold))
+                    .frame(width: 26, height: 18)
+                    .background(Color.afterBuildRed)
+                    .foregroundStyle(.white)
+                    .clipShape(Capsule())
+                    .offset(x: 20, y: -28)
+
+            }
+            Text(location.name)
+                .font(.caption)
+                .fontWeight(.semibold)
+        }
+    }
+}
+
+#Preview {
+    SpotAnnotation(location: SpotLocation(record: MockData.location))
+}
