7232676cb93cf4f0980dbc3bba08e781004aaafd
Ludovic HENRY <badsteam@hotmail.com>
2023-09-28 18:05:17 -0400

    removed the useless MapUserLocationButton on the TabMapView

--- AfterBuild/Views/TabMapView/TabMapView.swift
+++ AfterBuild/Views/TabMapView/TabMapView.swift
@@ -28,7 +28,6 @@ struct TabMapView: View {
                 UserAnnotation()
             }
             .mapControls {
-                MapUserLocationButton()
                 MapCompass()
                 MapScaleView()
             }
